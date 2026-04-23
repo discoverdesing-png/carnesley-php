420feedb966f08db037bd7232aa9770f1fe9e446
Pedro Simental Jacinto <discoverdesing@gmail.com>
2026-04-23 15:50:44 -0800

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,7 @@
 <?php
 $conn = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME'], $_ENV['DB_PORT']);
 if ($conn->connect_error) {
-    die("Error: " . $conn->connect_error);
+    die("Error de conexión: " . $conn->connect_error);
 }
+$conn->set_charset("utf8mb4");
+?>

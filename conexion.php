480185ba33c4ff14486f99e1eeb1c599e81ef1b3
Pedro Simental Jacinto <discoverdesing@gmail.com>
2026-04-23 15:35:42 -0800

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
-try {
-    $pdo = new PDO(
-        "mysql:host=".$_ENV['DB_HOST'].";port=".$_ENV['DB_PORT'].";dbname=".$_ENV['DB_NAME'],
-        $_ENV['DB_USER'],
-        $_ENV['DB_PASS']
-    );
-} catch (PDOException $e) {
-    die("Error: ". $e->getMessage());
+<?php
+$conn = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME'], $_ENV['DB_PORT']);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
 }
+
+$conn->set_charset("utf8mb4");
+?>

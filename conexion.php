66f537ca82046f98fa4b2ede0c96fe728d8bcc60
Pedro Simental Jacinto <discoverdesing@gmail.com>
2026-04-23 16:55:07 -0800

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,7 +1,18 @@
 <?php
-$conn = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME'], $_ENV['DB_PORT']);
+$host = getenv('DB_HOST'); // mysql.railway.internal
+$port = getenv('DB_PORT'); // 3306
+$db   = getenv('DB_NAME'); // railway
+$user = getenv('DB_USER'); // root
+$pass = getenv('DB_PASS'); // pLmtoCFCqHZkELifeKhvexrGqVjWpf1O
+
+// Para debug: descomenta esta línea si sigue fallando para ver qué variables llegan
+// die("HOST:$host DB:$db USER:$user PORT:$port");
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
 if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+    die("Error de conexión MySQL: " . $conn->connect_error);
 }
+
 $conn->set_charset("utf8mb4");
 ?>
